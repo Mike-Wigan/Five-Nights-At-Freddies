f9e42ca9a6e6ecf521d7950792b49bd6de0ecb05
michael wigan <michaelwigan3@gmail.com>
2024-06-20 23:19:10 -0600

    done

--- Five Nights At Freddies/loading screen.Designer.cs
+++ Five Nights At Freddies/loading screen.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // loading_screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "loading_screen";
+            this.Size = new System.Drawing.Size(360, 329);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
